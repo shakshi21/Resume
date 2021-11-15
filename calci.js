bf1080d50035d4e2ad3e4ef53b1f4942f98f5ddf
shakshi21 <93274543+shakshi21@users.noreply.github.com>
2021-11-15 20:07:02 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,20 @@
+
+
+
+//Demo01-> Custom module on node
+//Step1: create folder
+//step2: open terminal change directory to demo01>
+//step3: npm init -y
+
+
+function add(n1,n2){
+
+    return n1+n2;
+
+}
+
+const result=add(10,20);
+
+console.log("add(10,20) is "+result);
+
+
